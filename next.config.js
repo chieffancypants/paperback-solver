15540a1104c27a397e55668e5153edcd15b5299a
Wes Cruver <chieffancypants@gmail.com>
2023-07-11 16:06:38 -0700

    add repo as basepath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+// When building for production (Github Pages), add the repo name as the base path
+const basePath = process.env.NODE_ENV === 'production' ? '/paperback-solver' : ''
+
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-
+    basePath: basePath,
 }
 
 module.exports = nextConfig
